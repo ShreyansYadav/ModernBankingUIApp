6a7c683cbed85eb727577e6ef0db8266e406a18f
Shreyans yadav C# Microsoft <dev.shreyans16@gmail.com>
2026-01-31 18:07:34 +0300

    Add project files.

---
+++ ModernBankingUIApp/Core/ObservableObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ModernBankingApp.Core
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
