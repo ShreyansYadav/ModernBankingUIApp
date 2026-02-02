84019aaa2f981247e9ac790c4b1cac4358d83eea
Shreyans yadav C# Microsoft <dev.shreyans16@gmail.com>
2026-02-02 14:18:10 +0300

    UI update 2.0

---
+++ ModernBankingUIApp/MVVM/ViewModel/DiscoveryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModernBankingUIApp.MVVM.ViewModel
+{
+    class DiscoveryViewModel
+    {
+    }
+}
---
+++ ModernBankingUIApp/MVVM/ViewModel/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModernBankingUIApp.MVVM.ViewModel
+{
+    class HomeViewModel
+    {
+        // Add properties and logic as needed for your HomeViewModel
+    }
+}
